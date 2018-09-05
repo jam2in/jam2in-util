d51c43236d6e4f5d8c3f738e3f5c68bd531f823a
hjyun <hjyun@jam2in.com>
2018-09-05 16:32:20 +0900

    Fix scripts for lotte himart

--- make-zk-setup-script/make-zk-setup-script.bash
+++ make-zk-setup-script/make-zk-setup-script.bash
@@ -8,7 +8,7 @@ fi
 mkdir -p ${TARGET_DIR}
 
 echo "ZK_CLI=\"${ZK_CLI_STR}\"
-ZK_ADDR=\"-server ${ZK_ADDR_URL}:${ZK_PORT}\"
+ZK_ADDR=\"-server ${ZK_ENSEMBLE_ADDR}\"
 
 \$ZK_CLI \$ZK_ADDR create /arcus_repl 0
 
@@ -24,7 +24,7 @@ ZK_ADDR=\"-server ${ZK_ADDR_URL}:${ZK_PORT}\"
 \$ZK_CLI \$ZK_ADDR create /arcus_repl/group_list/${SERVICE_CODE} 0" > ${SETUP_FILE_NAME}
 
 echo "ZK_CLI=\"${ZK_CLI_STR}\"
-ZK_ADDR=\"-server ${ZK_ADDR_URL}:${ZK_PORT}\"" > ${REMOVE_FILE_NAME}
+ZK_ADDR=\"-server ${ZK_ENSEMBLE_ADDR}\"" > ${REMOVE_FILE_NAME}
 
 for((i=0;i<${GROUP_NUM};i++));
 do
@@ -41,6 +41,8 @@ do
 	for((j=0;j<${NODE_NUM};j++));
 	do
 		eval NODE_ADDR=\${GROUP${i}_NODE${j}_ADDR};
+        eval NODE_PORT=\${GROUP${i}_NODE_PORT};
+        eval GROUP_PORT=\${GROUP${i}_NODE_GROUP_PORT};
 		echo "
 \$ZK_CLI \$ZK_ADDR create /arcus_repl/cache_server_mapping/${NODE_ADDR}:${NODE_PORT} 0
 \$ZK_CLI \$ZK_ADDR create /arcus_repl/cache_server_mapping/${NODE_ADDR}:${NODE_PORT}/${SERVICE_CODE}^${GNAME}^${NODE_ADDR}:${GROUP_PORT} 0" >> ${SETUP_FILE_NAME}
@@ -49,9 +51,9 @@ do
 \$ZK_CLI \$ZK_ADDR delete /arcus_repl/cache_server_mapping/${NODE_ADDR}:${NODE_PORT}" >> ${REMOVE_FILE_NAME}
 
     eval START_MEMC_FILE_NAME_OUT=${START_MEMC_FILE_NAME}-${NODE_ADDR}.bash;
-    echo "mkdir -p ${ARCUS_INST_DIR}/memc_pid_list
+    echo "mkdir -p ${ARCUS_MEMCACHED_DIR}/memc_pid_list
 
-${ARCUS_INST_DIR}/bin/memcached -P ${ARCUS_INST_DIR}/memc_pid_list/memcached.${NODE_ADDR}:${NODE_PORT} -E ${ARCUS_INST_DIR}/lib/default_engine.so -X ${ARCUS_INST_DIR}/lib/syslog_logger.so -X ${ARCUS_INST_DIR}/lib/ascii_scrub.so -d -v -o 60 -r -R5 -U 0 -D: -b 8192 -m${NODE_MEM} -p ${NODE_PORT} -c ${MAX_CONNS} -t 6 -u ${MEMC_USER_ACCOUNT} -z ${ZK_ADDR_URL}:${ZK_PORT}" >> ${START_MEMC_FILE_NAME_OUT}
+${ARCUS_MEMCACHED_DIR}/memcached -P ${ARCUS_MEMCACHED_DIR}/memc_pid_list/memcached.${NODE_ADDR}:${NODE_PORT} -E ${ARCUS_MEMCACHED_LIB_DIR}/lib/default_engine.so -X ${ARCUS_MEMCACHED_LIB_DIR}/lib/syslog_logger.so -X ${ARCUS_MEMCACHED_LIB_DIR}/lib/ascii_scrub.so -d -v -o 60 -r -R5 -U 0 -D: -b 8192 -m${NODE_MEM} -p ${NODE_PORT} -c ${MAX_CONNS} -t 6 -u ${MEMC_USER_ACCOUNT} -z ${ZK_ENSEMBLE_ADDR}" >> ${START_MEMC_FILE_NAME_OUT}
 
   chmod +x ${START_MEMC_FILE_NAME_OUT}
 	done
